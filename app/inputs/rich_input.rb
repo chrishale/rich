bac044ff253dea8b6ae92d36fbb467f8313887e1
Chris Hale <chris@strobedigital.com>
2015-07-08 13:51:18 +0100

    lets use rich input

--- app/inputs/rich_input.rb
+++ app/inputs/rich_input.rb
@@ -1,19 +1,14 @@
-if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].version.version[0,1].to_i > 1)
+class RichInput < ::Formtastic::Inputs::TextInput  
+  def to_html
 
-  class RichInput < ::Formtastic::Inputs::TextInput  
-    def to_html
+    scope_type = object_name
+    scope_id = object.id
+    editor_options = Rich.options(options[:config], scope_type, scope_id)
 
-      scope_type = object_name
-      scope_id = object.id
-      editor_options = Rich.options(options[:config], scope_type, scope_id)
-
-      input_wrapping do
-        label_html <<
-        builder.text_area(method, input_html_options) <<
-        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe  
-      end
+    input_wrapping do
+      label_html <<
+      builder.text_area(method, input_html_options) <<
+      "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe  
     end
   end
-
 end
-
